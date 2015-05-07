92c428bb88fe5a358721136e2837efb1d79d0362
Cesar HERNANDEZ ANTONIO <hernandez.antonio.cesar19@gmail.com>
2015-05-07 15:43:22 +0200

    Deleted 'Translate default data in the database'

--- install/protected/install.sql
+++ install/protected/install.sql
@@ -235,6 +235,24 @@ CREATE TABLE `_DATABASE_NAME_`.`ImportLogPlatformLink` (
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 
+
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('JR1_R3', 'Journals (JR1) R3', 'Journal');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('JR1a_R3', 'Journals (JR1) R3 archive', 'Journal');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('JR1_R4', 'Journals (JR1) R4', 'Journal');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('JR1a_R4', 'Journals (JR1) R4 archive', 'Journal');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('BR1_R3', 'Books (BR1) R3', 'Book');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('BR1_R4', 'Books (BR1) R4', 'Book');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('BR2_R3', 'Book Sections (BR2) R3', 'Book');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('BR2_R4', 'Book Sections (BR2) R4', 'Book');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('DB1_R3', 'Database (DB1) R3', 'Database');
+INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('DB1_R4', 'Database (DB1) R4', 'Database');
+
+
+DELETE FROM `_DATABASE_NAME_`.Privilege;
+INSERT INTO `_DATABASE_NAME_`.Privilege (privilegeID, shortName) values (1, 'admin');
+INSERT INTO `_DATABASE_NAME_`.Privilege (privilegeID, shortName) values (2, 'add/edit');
+
+
 DELETE FROM `_DATABASE_NAME_`.Outlier;
 INSERT INTO `_DATABASE_NAME_`.Outlier (outlierID, outlierLevel, overageCount, overagePercent, color) values (1, 1, 50, 200, "yellow");
 INSERT INTO `_DATABASE_NAME_`.Outlier (outlierID, outlierLevel, overageCount, overagePercent, color) values (2, 2, 100, 300, "orange");
