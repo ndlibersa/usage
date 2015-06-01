e419b3a0d9332ef27cbb0413fe90c18e5914e78b
Cesar HERNANDEZ ANTONIO <hernandez.antonio.cesar19@gmail.com>
2015-06-01 16:17:00 +0200

    Deleted '_DATABASE_NAME_' in the update script (v1.1)

--- install/protected/update_1.1.sql
+++ install/protected/update_1.1.sql
@@ -1,5 +1,4 @@
-
-CREATE TABLE `_DATABASE_NAME_`.`Layout` (
+CREATE TABLE `Layout` (
   `layoutID` int(11) NOT NULL AUTO_INCREMENT,
   `layoutCode` varchar(45) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
@@ -7,48 +6,44 @@ CREATE TABLE `_DATABASE_NAME_`.`Layout` (
   PRIMARY KEY (`layoutID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
+INSERT INTO Layout (layoutCode, name, resourceType) values('JR1_R3', 'Journals (JR1) R3', 'Journal');
+INSERT INTO Layout (layoutCode, name, resourceType) values('JR1a_R3', 'Journals (JR1) R3 archive', 'Journal');
+INSERT INTO Layout (layoutCode, name, resourceType) values('JR1_R4', 'Journals (JR1) R4', 'Journal');
+INSERT INTO Layout (layoutCode, name, resourceType) values('JR1a_R4', 'Journals (JR1) R4 archive', 'Journal');
+INSERT INTO Layout (layoutCode, name, resourceType) values('BR1_R3', 'Books (BR1) R3', 'Book');
+INSERT INTO Layout (layoutCode, name, resourceType) values('BR1_R4', 'Books (BR1) R4', 'Book');
+INSERT INTO Layout (layoutCode, name, resourceType) values('BR2_R3', 'Book Sections (BR2) R3', 'Book');
+INSERT INTO Layout (layoutCode, name, resourceType) values('BR2_R4', 'Book Sections (BR2) R4', 'Book');
+INSERT INTO Layout (layoutCode, name, resourceType) values('DB1_R3', 'Database (DB1) R3', 'Database');
+INSERT INTO Layout (layoutCode, name, resourceType) values('DB1_R4', 'Database (DB1) R4', 'Database');
 
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('JR1_R3', 'Journals (JR1) R3', 'Journal');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('JR1a_R3', 'Journals (JR1) R3 archive', 'Journal');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('JR1_R4', 'Journals (JR1) R4', 'Journal');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('JR1a_R4', 'Journals (JR1) R4 archive', 'Journal');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('BR1_R3', 'Books (BR1) R3', 'Book');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('BR1_R4', 'Books (BR1) R4', 'Book');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('BR2_R3', 'Book Sections (BR2) R3', 'Book');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('BR2_R4', 'Book Sections (BR2) R4', 'Book');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('DB1_R3', 'Database (DB1) R3', 'Database');
-INSERT INTO `_DATABASE_NAME_`.Layout (layoutCode, name, resourceType) values('DB1_R4', 'Database (DB1) R4', 'Database');
+ALTER TABLE `ImportLog` ADD COLUMN `layoutCode` VARCHAR(45) NULL  AFTER `importDateTime` ;
+ALTER TABLE `ImportLog` ADD COLUMN `sushiServiceID` INT NULL  AFTER `loginID` ;
+ALTER TABLE `ImportLog` CHANGE COLUMN `fileName` `fileName` VARCHAR(145) NULL  ;
+ALTER TABLE `ImportLog` CHANGE COLUMN `archiveFileURL` `archiveFileURL` VARCHAR(145) NULL  ;
+ALTER TABLE `ImportLog` CHANGE COLUMN `logFileURL` `logFileURL` VARCHAR(145) NULL  ;
+ALTER TABLE `ImportLog` CHANGE COLUMN `details` `details` VARCHAR(245) NULL  ;
 
-ALTER TABLE `_DATABASE_NAME_`.`ImportLog` ADD COLUMN `layoutCode` VARCHAR(45) NULL  AFTER `importDateTime` ;
-ALTER TABLE `_DATABASE_NAME_`.`ImportLog` ADD COLUMN `sushiServiceID` INT NULL  AFTER `loginID` ;
-ALTER TABLE `_DATABASE_NAME_`.`ImportLog` CHANGE COLUMN `fileName` `fileName` VARCHAR(145) NULL  ;
-ALTER TABLE `_DATABASE_NAME_`.`ImportLog` CHANGE COLUMN `archiveFileURL` `archiveFileURL` VARCHAR(145) NULL  ;
-ALTER TABLE `_DATABASE_NAME_`.`ImportLog` CHANGE COLUMN `logFileURL` `logFileURL` VARCHAR(145) NULL  ;
-ALTER TABLE `_DATABASE_NAME_`.`ImportLog` CHANGE COLUMN `details` `details` VARCHAR(245) NULL  ;
-
-
-ALTER TABLE `_DATABASE_NAME_`.`Title` 
+ALTER TABLE `Title` 
 ADD COLUMN `resourceType` VARCHAR(45) NULL AFTER `title` ;
 
+UPDATE Title SET resourceType="Journal";
 
-UPDATE `_DATABASE_NAME_`.Title SET resourceType="Journal";
-
-ALTER TABLE `_DATABASE_NAME_`.`TitleISSN` 
+ALTER TABLE `TitleISSN` 
 CHANGE COLUMN `titleISSNID` `titleIdentifierID` INT(11) NOT NULL AUTO_INCREMENT  , 
 CHANGE COLUMN `issn` `identifier` VARCHAR(20) NULL DEFAULT NULL  , 
 CHANGE COLUMN `issnType` `identifierType` VARCHAR(20) NULL DEFAULT NULL  , 
-RENAME TO  `_DATABASE_NAME_`.`TitleIdentifier` ;
+RENAME TO  `TitleIdentifier` ;
 
-UPDATE `_DATABASE_NAME_`.TitleIdentifier SET identifierType="ISSN" where identifierType="print";
-UPDATE `_DATABASE_NAME_`.TitleIdentifier SET identifierType="eISSN" where identifierType="online";
+UPDATE TitleIdentifier SET identifierType="ISSN" where identifierType="print";
+UPDATE TitleIdentifier SET identifierType="eISSN" where identifierType="online";
 
-ALTER TABLE `_DATABASE_NAME_`.`MonthlyUsageSummary` ADD COLUMN `activityType` VARCHAR(45) NULL  AFTER `mergeInd` ;
-ALTER TABLE `_DATABASE_NAME_`.`MonthlyUsageSummary` ADD COLUMN `sectionType` VARCHAR(45) NULL  AFTER `activityType` ;
-ALTER TABLE `_DATABASE_NAME_`.`YearlyUsageSummary` ADD COLUMN `activityType` VARCHAR(45) NULL  AFTER `mergeInd` ;
-ALTER TABLE `_DATABASE_NAME_`.`YearlyUsageSummary` ADD COLUMN `sectionType` VARCHAR(45) NULL  AFTER `activityType` ;
+ALTER TABLE `MonthlyUsageSummary` ADD COLUMN `activityType` VARCHAR(45) NULL  AFTER `mergeInd` ;
+ALTER TABLE `MonthlyUsageSummary` ADD COLUMN `sectionType` VARCHAR(45) NULL  AFTER `activityType` ;
+ALTER TABLE `YearlyUsageSummary` ADD COLUMN `activityType` VARCHAR(45) NULL  AFTER `mergeInd` ;
+ALTER TABLE `YearlyUsageSummary` ADD COLUMN `sectionType` VARCHAR(45) NULL  AFTER `activityType` ;
 
-
-CREATE TABLE `_DATABASE_NAME_`.`SushiService` (
+CREATE TABLE `SushiService` (
   `sushiServiceID` int(11) NOT NULL AUTO_INCREMENT,
   `platformID` int(11) DEFAULT NULL,
   `publisherPlatformID` int(11) DEFAULT NULL,
@@ -68,13 +63,10 @@ CREATE TABLE `_DATABASE_NAME_`.`SushiService` (
   KEY `Index_platformID` (`platformID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `_DATABASE_NAME_`.`ImportLogPlatformLink` (
+CREATE TABLE `ImportLogPlatformLink` (
   `importLogPlatformLinkID` int(11) NOT NULL AUTO_INCREMENT,
   `platformID` int(11) DEFAULT NULL,
   `importLogID` int(11) DEFAULT NULL,
   PRIMARY KEY (`importLogPlatformLinkID`),
   KEY `Index_platformID` (`platformID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
-
-
-
