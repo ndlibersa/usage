b66fe15c420ab8f16a063c88d6872399a1eba068
Bradley Droubay <brad.droubay@sirsidynix.com>
2016-05-10 16:51:56 -0600

    Menu Fix

--- templates/header.php
+++ templates/header.php
@@ -245,7 +245,7 @@ else
 <?php
 //only show the 'Change Module' if there are other modules installed or if there is an index to the main CORAL page
 
-if ((file_exists($util->getCORALPath() . "index.php")) || ($config->settings->organizationsModule == 'Y') || ($config->settings->resourcesModule == 'Y') || ($config->settings->cancellationModule == 'Y') || ($config->settings->usageModule == 'Y')) {
+if ((file_exists($util->getCORALPath() . "index.php")) || ($config->settings->organizationsModule == 'Y') || ($config->settings->resourcesModule == 'Y') || ($config->settings->managementModule == 'Y') || ($config->settings->usageModule == 'Y')) {
 
 	?>
 
