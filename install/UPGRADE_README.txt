6a9dbb886eeb89939ab56925fba7a7773c8821b8
jeffnm <jeffmudge@gmail.com>
2016-10-21 09:53:04 -0500

    SQL Security Patch
    SQL injection security patch

--- install/UPGRADE_README.txt
+++ install/UPGRADE_README.txt
@@ -1,5 +1,7 @@
 	
-	
+1.2.1 Security Patch
+No database changes.    
+
 1.1 The 1.1 update to the CORAL Usage module includes a number of enhancements:
      
 	-Add support for JR1 R4 and JR1a R4, DB1, BR1, BR2
--- templates/footer.php
+++ templates/footer.php
@@ -7,7 +7,7 @@
 <div class="push">&nbsp;</div>
 </div>
 
-<div class="footer">Copyright &copy; 2015. Usage Statistics Module version 1.2<br/><a href="http://coral-erm.org/">CORAL Project Website</a> | <a href="https://github.com/ndlibersa/usage">GitHub Site</a></div>
+<div class="footer">Copyright &copy; 2015. Usage Statistics Module version 1.2.1<br/><a href="http://coral-erm.org/">CORAL Project Website</a> | <a href="https://github.com/ndlibersa/usage">GitHub Site</a></div>
 
 </body>
 </html>
